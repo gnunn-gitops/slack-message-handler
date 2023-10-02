77b1edf71507a4faff1f27e792ab1b33b67ccdff
Gerald Nunn <gerald.b.nunn@gmail.com>
2023-10-02 16:26:55 -0700

    Add get button to support direct click from Slack

--- src/main/java/com/redhat/demo/SlackMessageResource.java
+++ src/main/java/com/redhat/demo/SlackMessageResource.java
@@ -1,14 +1,18 @@
 package com.redhat.demo;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Map.Entry;
 import java.util.stream.Stream;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
@@ -26,8 +30,38 @@ public class SlackMessageResource {
 
     private static final Logger LOG = Logger.getLogger(SlackMessageResource.class);
 
+    private static final String GIT_URL = "https://github.com/gnunn-gitops/product-catalog";
+    private static final String GIT_SOURCE_URL = "https://github.com/gnunn-gitops/product-catalog-";
+    private static final String GIT_REVISION = "main";
+
     @RestClient
     PipelineService pipelineService;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public InputStream releaseApplication(@QueryParam("application") String application, @QueryParam("image") String image, @QueryParam("tag") String tag, @QueryParam("cluster") String cluster ) {
+        PushToProdPipeline msg = new PushToProdPipeline();
+        msg.application = application;
+        msg.imageURL = image;
+        msg.imageTag = tag;
+        msg.cluster = cluster;
+        msg.gitRevision = GIT_REVISION;
+        msg.gitURL = GIT_URL;
+        msg.gitSourceURL = GIT_SOURCE_URL + msg.application;
+
+        pipelineService.startPipeline(msg);
+        LOG.info("Start message sent to trigger: "+msg.toString());
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head><title>Starting Pipeline</title></head>");
+        sb.append("<body>");
+        sb.append("<span>Starting pipeline for " + application + " using image " + image + ":" + image + " in cluster " + cluster + "</span>");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return new ByteArrayInputStream(sb.toString().getBytes());
+    }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
@@ -40,7 +74,7 @@ public class SlackMessageResource {
                         .flatMap(SlackMessageResource::recursiveFlattening)
                         .filter(e -> "value".equals(e.getKey()))
                         .findAny();
-            
+
             if (result.isPresent()) {
                 String value = result.get().getValue().toString();
                 //LOG.infof("Value is %s", value);
@@ -50,15 +84,15 @@ public class SlackMessageResource {
                 msg.imageURL = values[1];
                 msg.imageTag = values[2];
                 msg.cluster = values[3];
-                msg.gitRevision = "main";
-                msg.gitURL = "https://github.com/gnunn-gitops/product-catalog";
-                msg.gitSourceURL = "https://github.com/gnunn-gitops/product-catalog-" + msg.application;
+                msg.gitRevision = GIT_REVISION;
+                msg.gitURL = GIT_URL;
+                msg.gitSourceURL = GIT_SOURCE_URL + msg.application;
 
                 pipelineService.startPipeline(msg);
                 LOG.info("Start message sent to trigger: "+msg.toString());
             } else {
                 LOG.info("No value found");
-                LOG.info(json.encodePrettily()); 
+                LOG.info(json.encodePrettily());
             }
 
         } else {
@@ -69,20 +103,20 @@ public class SlackMessageResource {
 
     private static Stream<Map.Entry<String, Object>> recursiveFlattening(
         Entry<String, Object> entry) {
-  
+
       Object jsonElement = entry.getValue();
-  
+
       if (jsonElement instanceof JsonObject) {
         return ((JsonObject) jsonElement).stream()
                                          .flatMap(SlackMessageResource::recursiveFlattening);
-  
+
       } else if (jsonElement instanceof JsonArray) {
         return ((JsonArray) jsonElement).stream()
                                         .map(v -> Map.entry("", v))
                                         .flatMap(SlackMessageResource::recursiveFlattening);
-  
+
       } else {
         return Stream.of(entry);
       }
-    }    
+    }
 }
